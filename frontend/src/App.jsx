19f0afa6a2ab2b2d1a66d272bffbe71cb1579296
smolvamp <kartik14704@gmail.com>
2025-05-24 00:41:25 +0530

    added frontend basic structure.

---
+++ frontend/src/App.jsx
@@ -0,0 +1,23 @@
+import {Box } from "@chakra-ui/react";
+import { Routes, Route } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import CreatePage from "./pages/CreatePage";
+import Navbar from "./components/ui/Navbar";
+
+
+function App() {
+ 
+  return (
+    <Box minH={"100vh"}>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/create" element={<CreatePage />} />
+        {/* <Route path="/contact" element={<Contact />} /> */}
+      </Routes>
+    </Box>
+  );
+   
+}
+
+export default App;
